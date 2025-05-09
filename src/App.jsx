2814ce13f369ee97eaa051aa7d9d03468c5a9b07
Benjamin Farthing <benjamin.farthing@gmail.com>
2025-05-09 13:33:20 -0400

    finished assignment with included css to make it more fun

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { useState } from "react";
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+import { tvShows } from "./shows/data.js";
+
 export default function App() {
+  //State to hold the selected show
+  const [selectedShow, setSelectedShow] = useState(null);
+
   return (
     <>
       <header>
-        <p>React TV</p>
+        <ShowSelection shows={tvShows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,23 @@
+//import React from "react"; NOT NEEDED BECAUSE NEVER CALLED FOR
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ shows, setSelectedShow }) {
+  const handleClick = (show) => {
+    //When a show is clicked, update the selected show state
+    setSelectedShow(show);
+  };
+
+  return (
+    <nav className="show-selection">
+      <h2>Select a Show</h2>
+      <div>
+        {shows.map((show) => (
+          <a key={show.name} className="show" onClick={() => handleClick(show)}>
+            {show.name}
+          </a>
+        ))}
+      </div>
+    </nav>
+  );
 }
